ecc4f3b1ead18bf5295598fd1284735ce0f8c3bd
Chandan U <chandanucodeid04@gmail.com>
2024-09-15 21:08:01 +0530

    Update p11a.c

--- Unix programs/program11/p11a.c
+++ Unix programs/program11/p11a.c
@@ -1,1 +1,29 @@
+#include <iostream>
+#include <pthread.h>
+
+void* countTo100(void* arg) {
+    int threadID = *(int*)arg; // Get the thread ID
+    for (int i = 1; i <= 100; ++i) {
+        std::cout << "Thread " << threadID << ": Count " << i << std::endl;
+    }
+    return nullptr; // End of thread function
+}
+
+int main() {
+    const int numThreads = 2;
+    pthread_t threads[numThreads];
+    int threadIDs[numThreads] = {1, 2}; // Thread IDs
+
+    // Create threads
+    for (int i = 0; i < numThreads; ++i) {
+        pthread_create(&threads[i], nullptr, countTo100, &threadIDs[i]);
+    }
+
+    // Wait for threads to finish
+    for (int i = 0; i < numThreads; ++i) {
+        pthread_join(threads[i], nullptr);
+    }
+
+    return 0;
+}
 
